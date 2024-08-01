48c1b345ac1a3d1541092c96499f10c58ca96ba6
Jesse Drelick <info@jessedrelick.com>
2024-08-01 14:05:59 -0400

    add hammox

--- mix.exs
+++ mix.exs
@@ -25,7 +25,8 @@ defmodule Agens.MixProject do
     [
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:bumblebee, "~> 0.5.3"},
-      {:exla, "~> 0.7.0"}
+      {:exla, "~> 0.7.0"},
+      {:hammox, "~> 0.7", only: :test}
     ]
   end
 
