9b284475f100e9cca0d25782e3f54faf4027f78a
Jesse Drelick <info@jessedrelick.com>
2026-05-07 11:47:41 -0400

    dynamic routing

--- lib/agens/router.ex
+++ lib/agens/router.ex
@@ -11,14 +11,22 @@ defmodule Agens.Router do
       alias Agens.Router.Output
       alias Agens.Message
 
-      def route(%Message{next: next}) when is_list(next) and next != [], do: next
-
       def route(%Message{} = message) do
         case prepare_outputs(message) do
           {:ok, merged} -> resolve(message, merged)
           {:error, _} -> [:retry]
         end
       end
+
+      def route(%Message{} = message, dynamic_next)
+          when is_list(dynamic_next) and dynamic_next != [] do
+        case Agens.Router.parse_next(dynamic_next) do
+          [] -> route(message)
+          next -> next
+        end
+      end
+
+      def route(%Message{} = message, _), do: route(message)
 
       defp prepare_outputs(%Message{outputs: outputs_result} = message) do
         outputs_def = outputs(message)
@@ -38,4 +46,18 @@ defmodule Agens.Router do
       defp prepare_outputs(_), do: {:error, :invalid_outputs}
     end
   end
+
+  @spec parse_next(list(map())) :: list()
+  def parse_next(next) when is_list(next) do
+    next
+    |> Enum.map(&do_parse/1)
+    |> Enum.reject(&is_nil/1)
+  end
+
+  defp do_parse(%{"type" => "route", "value" => value}), do: {:route, value, 1}
+  defp do_parse(%{"type" => "yield", "value" => value}), do: {:yield, value}
+  defp do_parse(%{"type" => "job", "value" => value}), do: {:job, value}
+  defp do_parse(%{"type" => "end"}), do: :end
+  defp do_parse(%{"type" => "retry"}), do: :retry
+  defp do_parse(_), do: nil
 end
